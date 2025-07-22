41110b600c3bdc0d95639e84fede7bec3b1d9fdf
Balarabe Nkom <bnkom@yuccantech.com>
2025-07-22 20:06:54 +1200

    Desktop scrolling replaced

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,13 +5,17 @@ export const About = (props) => {
     <div id="about">
       <div className="container">
         <div className="row">
+          <div className="col-xs-12">
+            <h2>About Us</h2>
+          </div>
+        </div>
+        <div className="row">
           <div className="col-xs-12 col-md-6">
             {" "}
             <img src="img/about.jpg" className="img-responsive about-image-enlarged" alt="" />{" "}
           </div>
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
-              <h2>About Us</h2>
               <p>{props.data ? props.data.paragraph : "loading..."}</p>
               <h3>Why Choose Us?</h3>
               <div className="list-style">
