1b355bf0b51e8f22e2e76b5effa7964cb60d7f25
Balarabe Nkom <bnkom@yuccantech.com>
2025-07-27 04:05:13 +1200

    Promo ON

--- src/config/promoConfig.js
+++ src/config/promoConfig.js
@@ -1,7 +1,7 @@
 // CENTRALIZED PROMO TOGGLE CONTROL
 // Set to true to enable both promotional banner and promos section
 // Set to false to disable both promotional banner and promos section
-export const PROMO_FEATURES_ENABLED = false;
+export const PROMO_FEATURES_ENABLED = true;
 
 // This single switch controls:
 // 1. Promotional banner (scrolling bar at top)
